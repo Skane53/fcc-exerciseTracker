0061325fc9824b27950a4a080318aa5fac857af0
Samba Kane <sambakane175@gmail.com>
2023-06-09 17:58:00 +0000

    Removing __v

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ app.get("/api/users/:_id/logs", (req, res) => {
     dataToReturn = [...data];
     dataToReturn[0]["log"] = logToReturn;
     dataToReturn[0]["count"] = logToReturn.length;
+    delete dataToReturn[0]["__v"];
 
     res.send(dataToReturn);
   });
