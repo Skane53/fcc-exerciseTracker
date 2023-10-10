4f6322945e6a0eaebca9bada5a1607cc66498647
Samba Kane <sambakane175@gmail.com>
2023-10-10 18:33:08 +0000

    Il reste que la gestion des erreurs des dates

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ app.post("/api/users/:_id/exercises", (req, res, next) => {
 
 app.get("/api/users/:_id/logs", (req, res) => {
   const _id = req.params._id;
-  const from = req.query.from && -Infinity; //new Date(req.query.from).getTime() || -Infinity;
-  const to = new Date(req.query.to).getTime() || Infinity;
+  const from = new Date(req.query.from).getTime() || Number.MIN_SAFE_INTEGER; //new Date(req.query.from).getTime() || -Infinity;
+  const to = new Date(req.query.to).getTime() || Number.MAX_SAFE_INTEGER;
   const limit = req.query.limit;
 
   ExerciseTracker.find(
@@ -87,19 +87,19 @@ app.get("/api/users/:_id/logs", (req, res) => {
     { "log._id": 0, __v: 0 },
     { "log.date": { $gt: from } }
   ).then((data) => {
-    console.log(
-      new Date(from).getTime(),
-      new Date(data[0]["log"][1]["date"]).getTime()
-    );
+    //console.log(from, new Date(data[0]["log"][1]["date"]).getTime());
     if (data.length == 0) return res.send("this _id is not in the database");
     let logToReturn = data[0]["log"];
     logToReturn = logToReturn.filter(
       (i) =>
-        Number(new Date(i["date"]).getTime()) >=
-          Number(new Date(from).getTime()) &&
-        Number(new Date(i["date"]).getTime()) <= Number(new Date(to).getTime())
+        Number(new Date(i["date"]).getTime()) >= from &&
+        Number(new Date(i["date"]).getTime()) <= to
     );
-    console.log(logToReturn);
+
+    let countToReturn = logToReturn.length;
+
+    data[0]["log"] = logToReturn;
+    data[0]["count"] = countToReturn;
     res.send(data[0]);
   });
 });
