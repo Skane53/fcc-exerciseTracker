7e9d3a7d372409c0c7b03b1fccdd53c0284d40c6
Samba Kane <sambakane175@gmail.com>
2023-10-13 16:33:47 +0000

    object conversion test 1

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ app.get("/api/users/:_id/logs", (req, res) => {
 
       data[0]["log"] = logToReturn;
       data[0]["count"] = countToReturn;
-      res.send(data[0]);
+      console.log({ ...data[0]["_doc"] });
+      res.send({ ...data[0]["_doc"] });
     })
     .catch((err) => {
       res.send("this _id is not in the database");
